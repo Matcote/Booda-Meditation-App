a8dc4ae8b4cf7822a9a5e92559837817197166c6
Matcote <matthewcotesipod@gmail.com>
2020-08-24 18:32:16 -0400

    login error message modal

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,12 +4,15 @@ import { useDispatch } from "react-redux";
 import { startLogIn, logIn, logInError } from "../actions";
 import { useHistory, Link } from "react-router-dom";
 import { FiLock, FiUser } from "react-icons/fi";
+import { IoIosWarning } from "react-icons/io";
 
 const Login = () => {
   const history = useHistory();
   const dispatch = useDispatch();
   const emailRef = React.createRef();
   const passwordRef = React.createRef();
+  const [modal, setModal] = React.useState("none");
+  const [errorMessage, setErrorMessage] = React.useState(null);
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -31,42 +34,95 @@ const Login = () => {
           history.push("/");
         } else {
           dispatch(logInError());
-          window.alert(data.data);
+          setErrorMessage(data.data);
+
+          setModal("flex");
         }
       });
   };
 
   return (
-    <Wrapper>
-      <h1>Zunity</h1>
-      <Input>
-        <FiUser size={"1.2em"} />
-        <input
-          type="text"
-          placeholder="Email"
-          ref={emailRef}
-          name="email"
-          required
-        />
-      </Input>
-      <Input>
-        <FiLock size={"1.2em"} />
-        <input
-          type="password"
-          placeholder="Password"
-          ref={passwordRef}
-          name="psw"
-          required
-        />
-      </Input>
-      <Button onClick={handleLogin}>Sign in</Button>
+    <>
+      <Wrapper>
+        <h1>Zunity</h1>
+        <Input>
+          <FiUser size={"1.2em"} />
+          <input
+            type="text"
+            placeholder="Email"
+            ref={emailRef}
+            name="email"
+            required
+          />
+        </Input>
+        <Input>
+          <FiLock size={"1.2em"} />
+          <input
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            name="psw"
+            required
+          />
+        </Input>
+        <Button onClick={handleLogin}>Sign in</Button>
 
-      <Footer>
-        Don't have an account? <Link to="/signup">Sign up</Link>
-      </Footer>
-    </Wrapper>
+        <Footer>
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </Footer>
+      </Wrapper>
+      <Modal style={{ display: modal }} onClick={() => setModal("none")}>
+        <div>
+          <IoIosWarning color={"#F65656"} size={"3em"} />
+          <p>{errorMessage}</p>
+          <div>Dismiss</div>
+        </div>
+      </Modal>
+    </>
   );
 };
+
+const Modal = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  div {
+    position: relative;
+    width: 280px;
+    height: 210px;
+    background-color: #f9f8fc;
+    opacity: 1;
+    border-radius: 8px;
+    padding: 12px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    p {
+      font-size: large;
+      margin-top: 15px;
+    }
+    div {
+      position: absolute;
+      bottom: 0px;
+      width: 100%;
+      height: 50px;
+      background-color: #f65656;
+      color: white;
+      display: flex;
+      justify-content: center;
+      border-radius: 0px;
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
+      cursor: pointer;
+    }
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
